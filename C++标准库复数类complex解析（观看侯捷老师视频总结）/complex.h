23f49ecb52339a88761619cd4e8e3571737f0f85
ModestBean <35394714+ModestBean@users.noreply.github.com>
2018-03-17 15:24:56 +0800

    houjie C++

---
+++ C++标准库复数类complex解析（观看侯捷老师视频总结）/complex.h
@@ -0,0 +1,43 @@
+﻿#ifndef COMPLEX_H
+#define COMPLEX_H
+#include <iostream>
+using namespace std;
+class complex
+{
+public:
+	complex(double r = 0, double i = 0) :re(r), im(i) {};
+	complex& operator += (const complex&);
+	double real() const { return re; }
+	double imag() const { return im; }
+private:
+	double re, im;
+	friend complex&  _doapl(complex*, const complex&);
+};
+inline complex&
+_doapl(complex* ths, const complex& r) {
+	ths->re += r.re;
+	ths->im += r.im;
+	return *ths;
+}
+inline complex&
+complex::operator += (const complex& r) {
+	return _doapl(this, r);
+}
+inline double
+imag(const complex& x)
+{
+	return x.imag();
+}
+
+inline double
+real(const complex& x)
+{
+	return x.real();
+}
+
+ostream&
+operator << (ostream& os, const complex& x)
+{
+	return os << '(' << real(x) << ',' << imag(x) << ')';
+}
+#endif
