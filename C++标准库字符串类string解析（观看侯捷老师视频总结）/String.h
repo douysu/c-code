8e89f67ade8a31e1e881421f0c068e144091f57c
ModestBean <35394714+ModestBean@users.noreply.github.com>
2018-03-19 10:27:15 +0800

    c++ string

---
+++ C++标准库字符串类string解析（观看侯捷老师视频总结）/String.h
@@ -0,0 +1,43 @@
+#ifndef _MY_STRING
+#define _MY_STRING
+class String
+{
+public:
+	String(const char * cstr=0);//满足 String s1("hellow")的构造方式
+	String(const String& str);//满足 String s1(s2)的构造方式
+	String& operator = (const String& str);//满足 s1=s2的方式 
+	~String();
+private:
+	char * m_data;//自己的数据数组
+};
+#include <cstring>
+inline String::String(const char * cstr)
+{
+	if (cstr) {
+		m_data = new char[strlen(cstr) + 1];
+		strcpy(m_data, cstr);
+	}else {
+		m_data = new char[1];
+		*m_data = '\0';
+	}
+}
+inline String::String(const String& str)
+{
+	m_data = new char[strlen(str.m_data) + 1];
+	strcpy(m_data, str.m_data);
+}
+inline String::~String()
+{
+	delete[]  m_data;
+}
+inline String& String::operator =(const String& str){
+	if(this==&str){//检测自我赋值 
+		return *this;
+	}
+	delete[] m_data;
+	m_data = new char[ strlen(str.m_data) + 1 ];
+   	strcpy(m_data, str.m_data);
+   	return *this;
+}
+#endif //_MY_STRING
+
