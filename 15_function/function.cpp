ecdc0060684f785a8b7e18fedffaa6765f308ae8
douysu <yindou97@163.com>
2021-11-27 09:09:41 +0800

    feat: 增加std::function

---
+++ 15_function/function.cpp
@@ -0,0 +1,99 @@
+//#define FUNCTION_POINT
+#define STD_FUNCTION
+
+#ifdef FUNCTION_POINT
+#include <cstdio>
+// 解释：http://c.biancheng.net/view/228.html
+int Max(int, int);  //函数声明
+
+int main(void)
+{
+    int(*p)(int, int);  //定义一个函数指针
+    int a, b, c;
+    p = Max;  //把函数Max赋给指针变量p, 使p指向Max函数
+    printf("please enter a and b:");
+    scanf_s("%d %d", &a, &b);
+    c = (*p)(a, b);  //通过函数指针调用Max函数
+    printf("a = %d\nb = %d\nmax = %d\n", a, b, c);
+    return 0;
+}
+
+int Max(int x, int y)  //定义Max函数
+{
+    int z;
+    if (x > y)
+    {
+        z = x;
+    }
+    else
+    {
+        z = y;
+    }
+    return z;
+}
+#endif // FUNCTION_PINT
+
+#ifdef STD_FUNCTION
+
+#include <iostream>
+#include <functional>
+
+// 解释：https://c-cpp.com/cpp/utility/functional/function.html
+
+struct Foo
+{
+    Foo(int num) : m_num(num) {}
+    void print_add(int i) const
+    {
+        std::cout << m_num + i << '\n';
+    }
+    int m_num;
+};
+
+void print_num(int i)
+{
+    std::cout << i << '\n';
+}
+
+struct PrintNum {
+    void operator()(int i) const
+    {
+        std::cout << i << '\n';
+    }
+};
+
+int main()
+{
+    // 存储自由函数
+    std::function<void(int)> f_display = print_num;
+    f_display(-9);
+
+    // 存储 lambda
+    std::function<void()> f_display_42 = []() { print_num(42); };
+    f_display_42();
+
+    // 存储到 std::bind 调用的结果
+    std::function<void()> f_display_31337 = std::bind(print_num, 31337);
+    f_display_31337();
+
+    // 存储到成员函数的调用
+    std::function<void(const Foo&, int)> f_add_display = &Foo::print_add;
+    const Foo foo(314159);
+    f_add_display(foo, 1);
+    f_add_display(314159, 1);
+
+    // 存储到数据成员访问器的调用
+    std::function<int(Foo const&)> f_num = &Foo::m_num;
+    std::cout << "m_num: " << f_num(foo) << '\n';
+
+    // 存储到成员函数及对象的调用
+    using std::placeholders::_1;
+    std::function<void(int)> f_add_display2 = std::bind(&Foo::print_add, foo, _1);
+    f_add_display2(2);
+
+    // 存储到函数对象的调用
+    std::function<void(int)> f_display_obj = PrintNum();
+    f_display_obj(18);
+}
+
+#endif // STD_FUNCTION
