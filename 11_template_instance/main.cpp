d9ef65de21edec0e10aac176a358a6a8e9e83cc7
douysu <yindou97@163.com>
2021-12-13 17:16:31 +0800

    faet: template 16.8

--- 11_template_instance/main.cpp
+++ 11_template_instance/main.cpp
@@ -29,7 +29,7 @@ int main()
 
 #endif // TEM_16_2
 
-//#define TEM_16_3
+// #define TEM_16_3
 #ifdef TEM_16_3
 // 因为Sales_data是自定义数据类型，没有对运算符<重载，导致错误error: no match for 'operator<'出现
 
@@ -54,7 +54,7 @@ int main()
 }
 #endif // TEM_16_3
 
-//#define TEM_16_4
+// #define TEM_16_4
 #ifdef TEM_16_4
 template<typename Iterator, typename Value>
 Iterator find(Iterator first, Iterator last, const Value& v)
@@ -114,7 +114,7 @@ int main()
 #endif // TEM_16_6
 
 // TEM_16_7
-#define TEM_16_7
+// #define TEM_16_7
 #ifdef TEM_16_7
 
 template <typename T, unsigned int N> constexpr
@@ -131,3 +131,59 @@ int main()
 }
 
 #endif // TEM_16_7
+
+// TEM_16_8
+// #define TEM_16_8
+#ifdef TEM_16_8
+// 因为大多数类只定义了 != 操作而没有定义 < 操作，使用 != 可以降低对要处理的类型的要求。
+#endif // TEM_16_8
+
+// TEM_16_9
+// #define TEM_16_9
+#ifdef TEM_16_9
+// 一个函数模版就是一个公式，可用来生成针对特定类型的函数版本。类模版是用来生成类的蓝图的，与函数模版的不同之处是，
+// 编译器不能为类模版推断模版参数类型。如果我们已经多次看到，为了使用类模版，我们必须在模版名后的尖括号中提供额外信息。
+#endif // TEM_16_9
+
+// TEM_16_10
+// #define TEM_16_10
+#ifdef TEM_16_10
+// 一个类模版的每个实例都形成一个独立的类。
+#endif // TEM_16_10
+
+// TEM_16_11
+// #define TEM_16_11
+#ifdef TEM_16_11
+
+template <typename elemType> class ListItem;
+template <typename elemType> class List {
+public:
+	List<elemType>();
+	List<elemType>(const List<elemType>&);
+	List<elemType>& operator=(const List<elemType>&);
+	~List();
+	void insert(ListItem<elemType>* ptr, elemType value);
+private:
+	ListItem<elemType>* front, * end;
+};
+
+#endif // TEM_16_11
+
+// TEM_16_12
+// #define TEM_16_12
+#ifdef TEM_16_12
+// Blob.h BlobPtr.h
+#endif // TEM_16_12
+
+// TEM_16_13
+// #define TEM_16_13
+#ifdef TEM_16_13
+
+#include "Blob.h"
+
+int main()
+{
+	return 0;
+}
+
+#endif // TEM_16_13
