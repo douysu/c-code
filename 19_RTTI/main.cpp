a1aa33740b18082967bb21c04f5c196715851ae4
douysu <yindou97@163.com>
2021-12-22 16:15:46 +0800

    feat: RTTI 19_10

--- 19_RTTI/main.cpp
+++ 19_RTTI/main.cpp
@@ -194,7 +194,7 @@ int main()
 // 在这个过程中会进行对象的判断，判断是否安全
 #endif
 
-#define PRACTICE_19_6
+// #define PRACTICE_19_6
 #ifdef PRACTICE_19_6
 #include <typeinfo>
 class A
@@ -241,6 +241,171 @@ int main()
 		cout << "same type as C" << endl;
 	if (typeid(*pa1) == typeid(C))
 		cout << "same type as C" << endl;
+
+	return 0;
+}
+
+#endif
+
+// #define PRACTICE_19_7
+#ifdef PRACTICE_19_7
+#include <typeinfo>
+class A
+{
+public:
+	virtual ~A() {}
+};
+
+class B : public A
+{};
+
+class C : public B
+{};
+
+class D : public B, public A
+{};
+
+int main()
+{
+	A* pa1 = new C();
+	if (C* qc = dynamic_cast<C*>(pa1))
+	{
+		cout << "success" << endl;
+	}
+	else
+	{
+		cout << "fail" << endl;
+	}
+
+	A* pa2 = new C();
+	try
+	{
+		const C& rc = dynamic_cast<const C&>(*pa2);
+		if (typeid(rc) == typeid(C))
+			cout << "same type as C" << endl;
+	}
+	catch (std::bad_cast& e)
+	{
+		cout << e.what() << endl;
+	}
+
+	C c;
+	if (typeid(*pa1) == typeid(*pa2))
+		cout << "same type" << endl;
+	if (typeid(*pa1) == typeid(c))
+		cout << "same type as C" << endl;
+	if (typeid(*pa1) == typeid(C))
+		cout << "same type as C" << endl;
+	
+	return 0;
+}
+#endif
+
+// #define PRACTICE_19_9
+#ifdef PRACTICE_19_9
+#include <typeinfo>
+
+class Base
+{
+	friend bool operator==(const Base&, const Base&);
+
+public:
+	Base() = default;
+	Base(int i_) : i(i_) {}
+
+protected:
+	virtual bool equal(const Base&) const;
+
+private:
+	int i;
+};
+
+class Derived : public Base
+{
+public:
+	Derived() = default;
+	Derived(std::string s_, int i_) : s(s_), Base(i_) { }
+
+protected:
+	bool equal(const Base&) const;
+
+private:
+	std::string s;
+};
+
+bool operator==(const Base& lhs, const Base& rhs)
+{
+	return typeid(lhs) == typeid(rhs) && lhs.equal(rhs);
+}
+
+bool Base::equal(const Base& rhs) const
+{
+	return this->i == rhs.i;
+}
+
+bool Derived::equal(const Base& rhs) const
+{
+	auto r = dynamic_cast<const Derived&>(rhs);
+	return (this->s == r.s) && this->Base::equal(rhs);
+}
+
+int main()
+{
+	Base* pb1 = new Derived();
+	Base* pb2 = new Derived();
+	Base* pb3 = new Derived("a", 1);
+	Base* pb4 = new Base();
+
+	std::cout << std::boolalpha << (*pb1 == *pb2) << std::endl;
+	std::cout << std::boolalpha << (*pb1 == *pb3) << std::endl;
+	std::cout << std::boolalpha << (*pb1 == *pb4) << std::endl;
+
+	int arr[10];
+	Derived d;
+
+	cout << typeid(42).name() << ", "
+		<< typeid(arr).name() << ", "
+		<< typeid(d).name() << ", "
+		<< typeid(std::string).name() << ", "
+		<< typeid(pb1).name() << ", "
+		<< typeid(*pb1).name() << ", "
+		<< typeid(*pb3).name() << endl;
+
+	return 0;
+}
+
+#endif
+
+#define PRACTICE_19_10
+#ifdef PRACTICE_19_10
+#include <typeinfo>
+
+class A
+{
+public:
+	virtual ~A() {}
+};
+
+class B : public A
+{};
+
+class C : public B
+{};
+
+int main(int argc, char const* argv[])
+{
+	// 最终答案和指针有关系
+
+	 //A *pa = new C;
+	 //std::cout << typeid(pa).name() << std::endl;
+
+	 //C cobj;
+	 //A &ra = cobj;
+	 //std::cout << typeid(&ra).name() << std::endl;
+
+	B* px = new B;
+	A& ra = *px;
+	std::cout << typeid(ra).name() << std::endl;
 
 	return 0;
 }
