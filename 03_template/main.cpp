0d1850ce587813c860c752693a67e1f92da571d7
douysu <yindou97@163.com>
2022-09-13 20:46:24 +0800

    feat: update template

--- 03_template/main.cpp
+++ 03_template/main.cpp
@@ -1,6 +1,12 @@
-﻿#include <iostream>
+﻿
+
+// #define TEST_1
+#ifdef TEST_1
+
+#include <iostream>
 #include "ClassTemplate.h"
 using namespace std;
+
 int main() {
 	ClassTemplate<int> s1(1,2);
 	ClassTemplate<float> s2(1.0f, 2.0f);
@@ -9,3 +15,54 @@ int main() {
 	cout << "计算出的int面积是" << area<< "计算出的float面积是" << areaf << endl;
 	std::cin.get();
 }
+#endif // !TEST_1
+
+#define TEST_2
+#ifdef TEST_2
+
+#include <iostream>
+
+template<class T1, class T2>
+class Foo {
+public:
+    void foo() {
+        printf("无特化 \n");
+    }
+};
+
+template<class T1>
+class Foo<T1, int> {
+public:
+    void foo() {
+        printf("偏特化 \n");
+    }
+};
+
+template<>
+class Foo<int, int> {
+public:
+    void foo() {
+        printf("全特化 \n");
+    }
+};
+
+template<class T>
+void f() { T a; }
+
+template<>
+//void f() { int a; } // 有歧义，无法推断T的类型
+void f<int>() { int a; } // 显示制定模板参数类型
+
+int main() {
+    Foo<int, double> foo1; // 无特化
+    Foo<double, int> foo2; // 偏特化
+    Foo<int, int> foo3; // 全特化
+
+    foo1.foo();
+    foo2.foo();
+    foo3.foo();
+
+    std::cin.get();
+}
+#endif // !TEST_1
+
